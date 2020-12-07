1c0f1757dbe5571dd145f290231516dc4e134534
AnastasiaGaranovich <nastyamadness@gmail.com>
2020-12-07 15:21:24 +0300

    Create phone call view with label and some buttons

---
+++ Homework_3/Homework_3/PhoneViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_3
+//
+//  Created by Анастасия Гаранович on 7.12.20.
+//
+
+import UIKit
+
+class PhoneViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
