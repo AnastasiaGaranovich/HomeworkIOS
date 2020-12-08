ed8275c3e6cdaeb5ee15307fa255a1a1dc3687d5
AnastasiaGaranovich <nastyamadness@gmail.com>
2020-12-08 15:36:12 +0300

    Make prototype view for constraints

---
+++ Homework_4/Homework_4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_4
+//
+//  Created by Анастасия Гаранович on 8.12.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
